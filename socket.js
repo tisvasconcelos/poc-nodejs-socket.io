8f31ef9af62a3424d6ac77ce6869983c4f436f2a
Tiago Vasconcelos <tisvasconcelos@gmail.com>
2013-09-27 15:24:23 -0300

    Alterações e gitignore

--- socket.js
+++ socket.js
@@ -3,9 +3,7 @@ var http = require('http').createServer(index),
     fs = require('fs');
 
 http.listen('8080');
-
 function index(req,res){
-
 	fs.readFile(__dirname + '/index.html', function(err, data){
 		if(err){
 			res.writeHead(500);
@@ -15,22 +13,23 @@ function index(req,res){
 		res.writeHead(200);
 		res.end(data);
 	});
-
 }
 
-var visitas = 0;
+var view = 0;
 
 io.sockets.on('connection', function(socket){
+	socket.on('put', function (data) {
+		view++;
 
-	socket.on('message', function(){
-		visitas++;
-		socket.emit('message', visitas);
-		socket.broadcast.emit('message', visitas);
+		socket.emit('put', {'view': view});
+		socket.broadcast.emit('put', {'view': view});
 	});
 
-	socket.on('disconnect', function(){
-		visitas--;
-		socket.broadcast.emit('message', visitas);
-	});
+	socket.on('disconnect', function(object){
+		view--;
 
+		socket.broadcast.emit('put', {'view': view});
+	});
 });
+
+//connection.end();
